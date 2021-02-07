02c8ac1e7b4630496eaf8fdc86e656fdf5bfa9f2
JolyneKo <jolynek4444@protonmail.com>
2021-02-07 12:54:30 -0400

    JSON
    Estudando JSON

---
+++ Javascript/Tutorial/13 - JSON.js
@@ -0,0 +1,27 @@
+// JSON - JavaScript Object Notation
+
+// É usado para transportar dados
+// Várias línguages podem ler JSON
+// Arquivos '.json' usam a notação de objeto, e são parecidos com ele
+// A diferença, que não aceita funções e as propriedades são escritas em string de áspas duplas
+
+// JSON aceita: String, Number, Boolean, Object, Array
+
+// Exemplo Arquivo JSON:
+//
+// {
+//  "nome": "João",
+//  "idade": 23,
+//  "moradia": {
+//      "cidade": "Belem",
+//      "estado": "Pará",
+//      "país": "Brasil"
+//  },
+//  "hobbies": ["beber", "jogar", "dormir"]
+// }
+//
+// Para transformar um objeto em JSON, usa o método
+// JSON.stringify();
+//
+// Para transformar JSON em objeto, usa o método
+// JSON.parse();
