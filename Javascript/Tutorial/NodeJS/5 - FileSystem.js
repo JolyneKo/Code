eff71e449b08eb169ba96f43857b85ca6d9e05af
JolyneKo <jolynek4444@protonmail.com>
2021-02-09 12:56:43 -0400

    Aprendendo NodeJS
    FS

--- Javascript/Tutorial/NodeJS/5 - FileSystem.js
+++ Javascript/Tutorial/NodeJS/5 - FileSystem.js
@@ -17,9 +17,50 @@ fs.readFile('arquivo.txt', (error, conteúdo) => { // Ler o Arquivo, e executa a
 // Sync
 fs.writeFileSync('arquivoNovo.txt', 'conteúdo'); // Cria arquivo, e escreve nele
 
+// Async
 fs.writeFile('arquivoNovo.txt', 'conteúdo', (error) => { // Cria arquivo, escreve e executa a função
     if (error) throw error;
     console.log('Arquivo criado');
 })
 
-// 
+// Deletar arquivos
+
+fs.unlink('arquivo.txt');
+
+// Criar Arquivos
+fs.createWriteStream('arquivo.mp4');
+
+// Anexar texto ao arquivo
+
+// Anexa texto ao arquivo, cria arquivo se não existir
+
+// Sync
+fs.appendFileSync('arquivo.txt', 'texto');
+
+// Async
+fs.appendFile('arquivo.txt', 'texto', function(error) {
+    if (error) throw error;
+    console.log('Anexado texto ao arquivo');
+});
+
+// Diretórios
+
+// Criar Diretórios
+
+// Sync
+fs.mkdirSync('diretório');
+
+// Async
+fs.mkdir('diretório', function() {
+    console.log('Diretório criado');
+});
+
+// Deletar diretórios
+
+// Sync
+fs.rmdirSync('diretório');
+
+// Async
+fs.rmdir('diretório', function() {
+    console.log('Diretório removido');
+});
