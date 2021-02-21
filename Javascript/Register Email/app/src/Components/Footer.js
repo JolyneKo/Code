d0af0d33735431111c1ea96301e4cf6a640c9f8d
JolyneKo <jolynek4444@protonmail.com>
2021-02-21 16:07:20 -0400

    Site finalizado

--- Javascript/Register Email/app/src/Components/Footer.js
+++ Javascript/Register Email/app/src/Components/Footer.js
@@ -1,7 +1,43 @@
+import logo from '../images/logo/logo.png';
+
 function Footer() {
   return (
     <footer className="Footer">
+      <div className="coluna">
+        <h2>Alguma coisa</h2>
+        <ul>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+        </ul>
+      </div>
+      <div className="coluna">
+        <h2>Alguma coisa</h2>
+        <ul>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+        </ul>
+      </div>
+      <div className="coluna">
+        <h2>Alguma coisa</h2>
+        <ul>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+          <li>Blah</li>
+        </ul>
+      </div>
       
+      <div className="colunaFinal">
+        <img src={logo} alt="logo"/>
+        <p class="footer_copy">&copy; Site do João - 2021</p>
+      </div>
     </footer>
   )
 }
