2a31064dfe5143262b910dd04442e2a03e07c64f
JolyneKo <jolynek4444@protonmail.com>
2021-03-03 15:29:31 -0400

    Usando Jquery

--- HTML/Teste site 26/public/JS/index.js
+++ HTML/Teste site 26/public/JS/index.js
@@ -1,51 +1,26 @@
-const icon = document.querySelector('.header__icons__theme');
-const main = document.querySelector('.main');
-const cards = document.querySelectorAll('.card');
-const headerA = document.querySelectorAll('.header a');
-
 let isLightTheme = false;
 
-icon.addEventListener('click', () => {
-  // IF lightTheme, turn to Dark Theme. Else turn to Light Theme
-  (isLightTheme) ? turnDarkTheme() : turnLightTheme();
-});
+// Se for Light-theme, vira Dark-theme, e vice-versa.
+$('.header__icons__theme').on('click', () => isLightTheme ? turnDarkTheme() : turnLightTheme());
 
 // Turn to Dark theme
 function turnDarkTheme() {
-  main.style.backgroundColor = 'rgb(15, 15, 15)';
-
-  cards.forEach(card => {
-    const cardP = card.querySelector('.card__description')
-    card.style.backgroundColor = 'black';
-    card.style.boxShadow = '0 0 20px -15px rgba(255, 255, 255, 0.5)';
-    cardP.style.color = 'white';
-  });
-
-  headerA.forEach(a => {
-    a.style.color = 'white';
-  });
-
-  icon.innerHTML = '<i class="far fa-sun header__switch__theme style="color: white;""></i>'
-
+  $('.main').css('background-color', 'rgb(15, 15, 15)');
+  $('.card').css('background-color', 'black');
+  $('.card').css('box-shadow', '0 0 20px -15px rgba(255, 255, 255, 0.5)');
+  $('.card__description').css('color', 'white');
+  $('.header a').css('color', 'white');
+  $('.header__icons__theme').html('<i class="far fa-sun"></i>').css('color', 'white');
   isLightTheme = false;
 }
 
 // Turn to Light Theme
 function turnLightTheme() {
-  main.style.backgroundColor = '#f3f3f3';
-
-  cards.forEach(card => {
-    const cardP = card.querySelector('.card__description');
-    card.style.backgroundColor = 'white';
-    card.style.boxShadow = '0 0 20px -15px rgba(0, 0, 0, 0.5)';
-    cardP.style.color = 'black';
-  });
-
-  headerA.forEach(a => {
-    a.style.color = 'black';
-  });
-
-  icon.innerHTML = '<i class="far fa-moon" style="color: black;""></i>';
-
+  $('.main').css('background-color', '#f3f3f3');
+  $('.card').css('background-color', 'white');
+  $('.card').css('box-shadow', '0 0 20px -15px rgba(0, 0, 0, 0.5');
+  $('.card__description').css('color', 'black');
+  $('.header a').css('color', 'black');
+  $('.header__icons__theme').html('<i class="far fa-moon"></i>').css('color', 'black');
   isLightTheme = true;
 }
