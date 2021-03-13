523ff06521eed7b03f3a23fcdaef34cee7fb6469
JolyneKo <jolynek4444@protonmail.com>
2021-03-12 21:25:01 -0400

    Teste Frontend/backend

---
+++ PHP/Testes/Teste 07/public/index.php
@@ -0,0 +1,23 @@
+<?php
+$username = "jolyneko";
+$password = "JEanluc2021;";
+$conn = new mysqli("localhost", $username, $password, "Produtos01");
+
+$conn
+?>
+
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Produtos</title>
+</head>
+<body>
+    <ul class="produtos">
+        <?php ?>
+    </ul>
+</body>
+</html>
